d53bfc2647b4f03e3f86713fff20497379bdf258
Sam <sam82520.work@gmail.com>
2019-12-24 01:12:32 +0800

    解決第一次渲染時也會執行setVisible

--- src/components/HomePage/ThemeContext.js
+++ src/components/HomePage/ThemeContext.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Container from "./Container";
 import video from "../loop_home.mp4";
 import { ThemeContext } from "./theme-context";
@@ -12,11 +12,32 @@ const initTheme = {
   slider: "center"
 };
 
+const useVisibleEffect = (SetVisible, currentTheme) => {
+  const didMountRef = React.useRef(false);
+  useEffect(() => {
+    if (didMountRef.current) {
+      SetVisible();
+    } else {
+      didMountRef.current = true;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentTheme]);
+};
+
 export default () => {
   const [currentTheme, setTheme] = React.useState(initTheme);
+  const [visible, setVisible] = React.useState(0.4);
+
+  const SetVisible = () => {
+    setVisible(1);
+  };
+
+  useVisibleEffect(SetVisible, currentTheme);
 
   return (
-    <ThemeContext.Provider value={{ currentTheme, setTheme }}>
+    <ThemeContext.Provider
+      value={{ currentTheme, setTheme, visible, setVisible }}
+    >
       <Container />
     </ThemeContext.Provider>
   );
